7152259795ce7867172f1849921fb2f119fef315
SixingChen028 <sc10264@nyu.edu>
2025-01-30 23:23:56 -0500

    Initial commit

---
+++ environment.py
@@ -0,0 +1,249 @@
+import numpy as np
+import random
+
+import gymnasium as gym
+from gymnasium import Wrapper 
+from gymnasium.spaces import Box, Discrete
+
+
+class HarlowEnv(gym.Env):
+    """
+    A Harlow environment.
+    """
+
+    metadata = {'render_modes': ['human', 'rgb_array']}
+
+    def __init__(
+            self,
+            num_trials = 20,
+            flip_prob = 0.2, 
+            seed = None,
+        ):
+
+        """
+        Construct an environment.
+        """
+
+        self.num_trials = num_trials # max number of trials per episode
+        self.flip_prob = flip_prob # flip probability
+
+        # set random seed
+        self.set_random_seed(seed)
+
+        # initialize action and observation spaces
+        self.action_space = Discrete(3) # action 0, 1, 2 means choosing left, choosing right, fixating, respectively
+        self.observation_space = Box(low = -np.inf, high = np.inf, shape = (1,))
+
+
+    def reset(self):
+        """
+        Reset the environment.
+        """
+
+        # reset the environment
+        self.num_completed = 0 # number of trials completed
+        self.stage = 'fixation' # set the initial stage to fixation
+        self.correct_answer = np.random.randint(0, 2) # randomly pick a correct answer
+
+        obs = np.array([1.]) # use 1 to represent fixation stage
+        
+        info = {
+            'correct_answer': self.correct_answer, # include the correct answer in the information
+        }
+
+        return obs, info
+    
+
+    def step(self, action):
+        """
+        Step the environment.
+        """
+
+        done = False # initialize to False
+
+        # fixation stage
+        if self.stage == 'fixation':
+            # fixation action
+            if action == 2:
+                reward = 0.
+            
+            # decision action
+            else:
+                reward = -1. # give a panelty if the agent doesn't execute the fixation action
+                
+            self.stage = 'decision' # set the stage to decision
+            
+            obs = np.array([0.]) # use 0 to represent decision stage 
+        
+        # decision stage
+        elif self.stage == 'decision':
+            self.num_completed += 1 # update number of completed trials
+            self.flip_bandit() # randomly flip the bandit
+
+            if action == self.correct_answer:
+                reward = 1.
+            else:
+                reward = -1.
+            
+            self.stage = 'fixation' # set the stage to fixation
+            
+            obs = np.array([1.])
+        
+        # end the episode
+        if self.num_completed >= self.num_trials:
+            done = True
+        
+        info = {
+            'correct_answer': self.correct_answer,
+        }
+
+        return obs, reward, done, False, info
+    
+
+    def flip_bandit(self):
+        """
+        Flip the bandit.
+        """
+
+        if np.random.random() < self.flip_prob:
+            self.correct_answer = 1 - self.correct_answer
+    
+
+    def set_random_seed(self, seed):
+        """
+        Set random seed.
+        """
+
+        if seed is not None:
+            np.random.seed(seed)
+            random.seed(seed)
+
+    
+    def one_hot_coding(self, num_classes, labels = None):
+        """
+        One-hot code nodes.
+        """
+
+        if labels is None:
+            labels_one_hot = np.zeros((num_classes,))
+        else:
+            labels_one_hot = np.eye(num_classes)[labels]
+
+        return labels_one_hot
+
+
+
+class MetaLearningWrapper(Wrapper):
+    """
+    A meta-RL wrapper.
+    """
+
+    metadata = {'render_modes': ['human', 'rgb_array']}
+
+    def __init__(self, env):
+        """
+        Construct an wrapper.
+        """
+
+        super().__init__(env)
+
+        self.env = env
+        self.one_hot_coding = env.get_wrapper_attr('one_hot_coding')
+
+        self.init_prev_variables()
+
+        new_observation_shape = (
+            self.env.observation_space.shape[0] +
+            self.env.action_space.n + # previous action
+            1, # previous reward
+        )
+        self.observation_space = Box(low = -np.inf, high = np.inf, shape = new_observation_shape)
+
+
+    def step(self, action):
+        """
+        Step the environment.
+        """
+
+        obs, reward, done, truncated, info = self.env.step(action)
+
+        obs_wrapped = self.wrap_obs(obs)
+
+        self.prev_action = action
+        self.prev_reward = reward
+
+        return obs_wrapped, reward, done, truncated, info
+    
+
+    def reset(self, seed = None, options = {}):
+        """
+        Reset the environment.
+        """
+
+        obs, info = self.env.reset()
+
+        self.init_prev_variables()
+
+        obs_wrapped = self.wrap_obs(obs)
+
+        return obs_wrapped, info
+    
+
+    def init_prev_variables(self):
+        """
+        Reset action and reward from the previous trial.
+        """
+
+        self.prev_action = None
+        self.prev_reward = 0.
+
+
+    def wrap_obs(self, obs):
+        """
+        Wrap observation with action and reward from the previous trial.
+        """
+
+        obs_wrapped = np.hstack([
+            obs,
+            self.one_hot_coding(num_classes = self.env.action_space.n, labels = self.prev_action),
+            self.prev_reward
+        ])
+        return obs_wrapped
+
+
+
+if __name__ == '__main__':
+    # testing
+    
+    env = HarlowEnv()
+    env = MetaLearningWrapper(env)
+    
+
+    # model = RecurrentPPO(
+    #     policy = 'MlpLstmPolicy',
+    #     env = env,
+    #     verbose = 1,
+    #     learning_rate = 1e-4,
+    #     n_steps = 20,
+    #     gamma = 0.9,
+    #     ent_coef = 0.05,
+    # )
+
+    # model.learn(total_timesteps = 1000000)
+
+    for i in range(50):
+
+        obs, info = env.reset()
+        print('initial obs:', obs)
+        done = False
+        
+        while not done:
+            action = env.action_space.sample()
+            obs, reward, done, truncated, info = env.step(action)
+            print(
+                'obs:', obs, '|',
+                'action:', action, '|',
+                'correct answer:', info['correct_answer'], '|',
+                'reward:', reward, '|',
+                'done:', done, '|',
+            )
---
+++ training.py
@@ -0,0 +1,92 @@
+import os
+import argparse
+import gymnasium as gym
+import numpy as np
+import matplotlib.pyplot as plt
+
+from environment import *
+from network import *
+from a2c import *
+
+
+# note: vectorizing wrapper only works under this protection
+if __name__ == '__main__':
+
+    # parse args
+    parser = argparse.ArgumentParser()
+
+    # job parameters
+    parser.add_argument('--jobid', type = str, default = '0', help = 'job id')
+    parser.add_argument('--path', type = str, default = os.path.join(os.getcwd(), 'results'), help = 'path to store results')
+
+    # nework parameters
+    parser.add_argument('--hidden_size', type = int, default = 32, help = 'lstm hidden size')
+
+    # environment parameters
+    parser.add_argument('--num_trials', type = int, default = 20, help = 'number of trials per episode')
+    parser.add_argument('--flip_prob', type = float, default = 0.2, help = 'flip probability')
+
+    # training parameters
+    parser.add_argument('--num_episodes', type = int, default = 40000, help = 'training episodes')
+    parser.add_argument('--lr', type = float, default = 1e-3, help = 'learning rate')
+    parser.add_argument('--batch_size', type = int, default = 16, help = 'batch_size')
+    parser.add_argument('--gamma', type = float, default = 0.9, help = 'temporal discount')
+    parser.add_argument('--lamda', type = float, default = 1.0, help = 'generalized advantage estimation coefficient')
+    parser.add_argument('--beta_v', type = float, default = 0.1, help = 'value loss coefficient')
+    parser.add_argument('--beta_e', type = float, default = 0.05, help = 'entropy regularization coefficient')
+    parser.add_argument('--max_grad_norm', type = float, default = 1.0, help = 'gradient clipping')
+
+    args = parser.parse_args()
+
+    # set experiment path
+    exp_path = os.path.join(args.path, f'exp_{args.jobid}')
+    if not os.path.exists(exp_path):
+        os.makedirs(exp_path)
+
+    # set environment
+    seeds = [random.randint(0, 1000) for _ in range(args.batch_size)]
+    env = gym.vector.AsyncVectorEnv([
+        lambda: MetaLearningWrapper(
+            HarlowEnv(
+                num_trials = args.num_trials,
+                flip_prob = args.flip_prob,
+                seed = seeds[i],
+            )
+        )
+        for i in range(args.batch_size)
+    ])
+
+    # set net
+    net = LSTMRecurrentActorCriticPolicy(
+        feature_size = env.single_observation_space.shape[0],
+        action_size = env.single_action_space.n,
+        hidden_size = args.hidden_size,
+    )
+
+    # set model
+    model = BatchMaskA2C(
+        net = net,
+        env = env,
+        lr = args.lr,
+        batch_size = args.batch_size,
+        gamma = args.gamma,
+        lamda = args.lamda,
+        beta_v = args.beta_v,
+        beta_e = args.beta_e,
+        max_grad_norm = args.max_grad_norm,
+    )
+
+    # train network
+    data = model.learn(
+        num_episodes = args.num_episodes,
+        print_frequency = 10
+    )
+
+    # save net and data
+    model.save_net(os.path.join(exp_path, f'net.pth'))
+    model.save_data(os.path.join(exp_path, f'data_training.p'))
+
+    # visualization
+    plt.figure()
+    plt.plot(np.array(data['episode_reward']).reshape(100, -1).mean(axis = 1))
+    plt.show()
